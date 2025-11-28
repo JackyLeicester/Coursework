0d2e7efab9369da9b011d0077aa4713ed1b22264
Syed Fasiuddin <sf403@student.le.ac.uk>
2025-11-28 20:52:13 +0000

    boilerplate code for lexer

--- src/lexer.py
+++ src/lexer.py
@@ -1,6 +1,25 @@
+from token import Token
+
+
 class Lexer:
     def __init__(self, input: str):
         self.input = input
+        self.position: int = 0
+        self.read_position: int = 0
+        self.ch = "\0"
+        self.read_char()
+
+    def read_char(self) -> None:
+        if self.read_position >= len(self.input):
+            self.ch = "\0"
+        else:
+            self.ch = self.input[self.read_position]
+
+        self.position = self.read_position
+        self.read_position += 1
+
+    def next_token(self) -> Token:
+        return Token.Eof
 
     def __repr__(self):
         return f"{type(self).__name__}()"
--- src/token.py
+++ src/token.py
@@ -2,4 +2,4 @@ from enum import Enum, auto
 
 
 class Token(Enum):
-    Assign = auto()
+    Eof = auto()
