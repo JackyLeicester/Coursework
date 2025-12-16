a03ad23750bc1fbf1e503c9636ded73c80c985ee
ARDEE3M <jlx1@student.le.ac.uk>
2025-12-16 16:50:49 +0000

    Made input test cases

--- src/evaluator.py
+++ src/evaluator.py
@@ -120,9 +120,9 @@ def _eval(node: Expression, env: Env) -> Any:
             print(*args, end="")
             return None
         elif name == "input":
-            if(len(args) > 0):
+            if len(args) > 0:
                 raise RuntimeEvaluationError("abs expects 0 or 1 arguments")
-            elif(len(args) == 0):
+            elif len(args) == 0:
                 return input()
             else:
                 return input(args[0])
