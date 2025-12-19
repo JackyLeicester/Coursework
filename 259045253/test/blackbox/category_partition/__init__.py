dee4ac0758ef346c06e090450c848c89da18e11b
Syed Fasiuddin <sf403@student.le.ac.uk>
2025-12-19 19:18:43 +0000

    fix: test file name convention

--- 259045253/test/blackbox/category_partition/__init__.py
+++ 259045253/test/blackbox/category_partition/__init__.py
@@ -2,6 +2,6 @@ import unittest
 
 if __name__ == "__main__":
     loader = unittest.TestLoader()
-    suite = loader.discover(".", pattern="*.py")
+    suite = loader.discover(".", pattern="*_test.py")
     runner = unittest.TextTestRunner()
     runner.run(suite)
