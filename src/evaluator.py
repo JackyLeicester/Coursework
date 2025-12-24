246f18e6c6d32a38bae8b32900df9cd76821c2c0
ARDEE3M <jlx1@student.le.ac.uk>
2025-12-24 00:52:52 +0000

    removed print

--- src/evaluator.py
+++ src/evaluator.py
@@ -63,10 +63,7 @@ def _get_var(env: Env, name: str) -> Any:
 
 
 def _declare_var(env: Env, name: str, value: Any, is_const: bool) -> Any:
-    print("declared variable")
-    if name in env[-1]:
-        print("name already bound")
-        if env[-1][name][1]:
+    if name in env[-1] and env[-1][name][1]:
             raise RuntimeEvaluationError(f"Cannot redeclare constant '{name}'")
     env[-1][name] = (value, is_const)
     return value
