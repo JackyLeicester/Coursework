6c2c2e2e6d721414c0d1572e05564a1adcbeea4a
Syed Fasiuddin <sf403@student.le.ac.uk>
2025-12-26 16:50:57 +0000

    test: whitebox testing for user story #1

---
+++ 259045253/test/whitebox/condition_testing/pass_filename_test.py
@@ -0,0 +1,40 @@
+import unittest
+import os
+
+from src import read_file_contents
+
+
+class TestReadFileContents(unittest.TestCase):
+    """
+    This class performs condition testing on the following:
+    - `read_file_contents` function
+
+    This corresponds to user story number: #1
+    <https://github.com/JackyLeicester/Coursework/issues/1>
+    """
+
+    def setUp(self):
+        self.file_name = "test_file.txt"
+        self.test_file_contents = "This is a test file."
+        with open(self.file_name, "w") as f:
+            f.write(self.test_file_contents)
+
+    def tearDown(self):
+        if os.path.exists(self.file_name):
+            os.remove(self.file_name)
+
+    def test_null(self):
+        self.assertRaises(ValueError, read_file_contents, None)
+
+    def test_empty_filename(self):
+        self.assertIsNone(read_file_contents(""))
+
+    def test_existing_filename(self):
+        self.assertEqual(self.test_file_contents, read_file_contents(self.file_name))
+
+    def test_non_existing_filename(self):
+        self.assertIsNone(read_file_contents("non-existent-file.txt"))
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ 259045253/test/whitebox/loop_testing/pass_filename_test.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+class TestReadFileContents(unittest.TestCase):
+    """
+    NOTE: Since there are no loops in the method involved in executing
+    `if` statement, there is nothing to test via loop testing and hence
+    this class is empty.
+
+    This class performs condition testing on the following:
+    - `read_file_contents` function
+
+    This corresponds to user story number: #1
+    <https://github.com/JackyLeicester/Coursework/issues/1>
+    """
+
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
