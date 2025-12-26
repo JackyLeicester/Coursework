76bd6507bd7f07bdae9978391da0f6612314b1ef
Syed Fasiuddin <sf403@student.le.ac.uk>
2025-12-26 16:56:33 +0000

    test: whitebox testing for user story #11

---
+++ 259045253/test/whitebox/loop_testing/cli_arg.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+class TestCliArgs(unittest.TestCase):
+    """
+    NOTE: Since there are no loops in the method involved in executing
+    `continue` statement, there is nothing to test via loop testing and hence
+    this class is empty.
+
+    This class performs loop testing on the following:
+    - Passing of cli arguments to the script as `arg`
+
+    This corresponds to user story number: #11
+    <https://github.com/JackyLeicester/Coursework/issues/11>
+    """
+
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
